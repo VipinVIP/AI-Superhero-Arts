8a5cbdf9478b1a98d49956db88e41369f28c4ea0
VIPIN K P <58673683+VipinVIP@users.noreply.github.com>
2023-05-31 22:03:26 +0530

    Add Readme and Final Touchup ✨

---
+++ README.md
@@ -0,0 +1,28 @@
+# AI Superhero Arts App
+
+A simple android app made in Jetpack Compose as a part of MuLearn Android Compose BootCamp .
+
+### Screenshots
+
+<table>
+  <thead>
+    <tr>
+      <th> Dark Mode</th>
+      <th> Light mode</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><img src = "https://github.com/VipinVIP/AI-Superhero-Arts/assets/58673683/eacb7ccf-8d6d-43a2-8947-fcfed26e8354"></td>
+      <td><img src = "https://github.com/VipinVIP/AI-Superhero-Arts/assets/58673683/cf1c0f57-36e8-4033-8367-a927caa059a1"></td>
+    </tr>
+    <tr>
+      <td><img src = "https://github.com/VipinVIP/AI-Superhero-Arts/assets/58673683/6ae89148-afc5-405c-abcf-18ac0942db79"></td>
+      <td><img src = "https://github.com/VipinVIP/AI-Superhero-Arts/assets/58673683/e69e4117-da6d-41e2-a19c-2ffe9b5af22e"></td>
+    </tr>
+  </tbody>
+</table>
+
+### Picture credits
+- https://twitter.com/midj_couple
+- https://twitter.com/brauliobaltazar
--- app/src/main/java/com/example/aisuperheroarts/MainActivity.kt
+++ app/src/main/java/com/example/aisuperheroarts/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -81,7 +84,8 @@ fun ArtsContainer( modifier: Modifier = Modifier) {
     Column(
         modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.CenterHorizontally) {
+            .background(MaterialTheme.colorScheme.background)
+            .verticalScroll(rememberScrollState(),true), horizontalAlignment = Alignment.CenterHorizontally) {
         Column(
             Modifier
 
@@ -102,14 +106,16 @@ fun ArtsContainer( modifier: Modifier = Modifier) {
             Modifier
                 .width(400.dp)
                 .padding(10.dp)
+                .clip(RoundedCornerShape(10.dp))
                 .background(MaterialTheme.colorScheme.primaryContainer))
         {
-            Text(text = stringResource(titleString), fontSize = 35.sp ,modifier=Modifier.padding(10.dp))
+            Text(text = stringResource(titleString), fontSize = 35.sp ,modifier=Modifier.padding(10.dp), color = MaterialTheme.colorScheme.onPrimaryContainer)
 
-                Text(text = stringResource(descriptionString), fontWeight = FontWeight.Bold,modifier=Modifier.padding(10.dp))
+                Text(text = stringResource(descriptionString), fontWeight = FontWeight.Bold,modifier=Modifier.padding(10.dp),color = MaterialTheme.colorScheme.onPrimaryContainer)
 
 
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Row(
             Modifier
                 .fillMaxWidth()
@@ -118,12 +124,12 @@ fun ArtsContainer( modifier: Modifier = Modifier) {
             Button(onClick = { decrement() },modifier= Modifier
                 .width(150.dp)
                 .height(50.dp)) {
-                Text(text = "Previous",fontSize = 20.sp)
+                Text(text = "Previous",fontSize = 20.sp,color = MaterialTheme.colorScheme.onPrimary)
             }
             Button(onClick = { increment() },modifier= Modifier
                 .width(150.dp)
                 .height(50.dp)) {
-                Text(text = "Next",fontSize = 20.sp)
+                Text(text = "Next",fontSize = 20.sp,color = MaterialTheme.colorScheme.onPrimary)
             }
         }
     }
